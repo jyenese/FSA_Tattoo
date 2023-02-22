c520a29e6fd7860c70d861319df17a3ed3f038b7
Jye Calder-Mason <jyenese@hotmail.com>
2023-02-22 16:12:22 +1100

    Refactor server to use environment variables for port and database connection

---
+++ src/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+require("dotenv").config()
+
+const userRouters = require('./controllers/users/userRoutes.js');
+const bookingRouter = require('./controllers/booking/bookingRoutes.js');
+const showcaseRouter = require('./controllers/showcase/showcaseRoutes.js');
+const communityRouter = require('./controllers/community/communityRoutes.js');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(helmet())
+
+const corsOptions = {
+    origin: ["http://localhost:3000"],
+    optionSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions));
+
+app.get("/", (req, res) => {
+    res.json({
+        data: "Data has been sent"
+    })
+})
+
+app.use("/bookings", bookingRouter);
+app.use("/login", userRouters);
+app.use("/showcase", showcaseRouter);
+app.use("/community", communityRouter);
+
+module.exports = {
+    app,
+    PORT
+}
